88d6441a83833070f20095f567274f7c35992010
Marcos Brito <marcos.brito0507@gmail.com>
2024-04-14 18:57:14 -0300

    remove a criação das pk's do arquivo de create

--- src/create.sql
+++ src/create.sql
@@ -1,48 +1,39 @@
 CREATE TABLE armas (
-    arm_id NUMBER(10) NOT NULL,
     arm_nome VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE estados (
-    std_id NUMBER(10) NOT NULL,
     std_nome VARCHAR(18) NOT NULL
 );
 
 CREATE TABLE cidades (
-    cdd_id NUMBER(10) NOT NULL,
     cdd_nome VARCHAR(18) NOT NULL,
     cdd_std_id NUMBER(10) NOT NULL
 );
 
 CREATE TABLE tipos_de_crime (
-    tcr_id NUMBER(10) NOT NULL,
     tcr_tipo VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE tipos_de_agencia (
-    tag_id NUMBER(10) NOT NULL,
     tag_tipo VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE agencias (
-    agn_id NUMBER(10) NOT NULL,
     agn_cod CHAR(7) NOT NULL,
     agn_nome VARCHAR(24) NOT NULL,
     agn_tag_id NUMBER(10) NOT NULL
 );
 
 CREATE TABLE racas (
-    rac_id NUMBER(10) NOT NULL,
     rac_nome VARCHAR(18) NOT NULL
 );
 
 CREATE TABLE etnias (
-    etn_id NUMBER(10) NOT NULL,
     etn_nome VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE vitimas (
-    vtm_id NUMBER(10) NOT NULL,
     vtm_sexo CHAR(6),
     vtm_idade NUMBER(3) NOT NULL,
     vtm_rac_id NUMBER(10) NOT NULL,
@@ -50,7 +41,6 @@ CREATE TABLE vitimas (
 );
 
 CREATE TABLE infratores (
-    inf_id NUMBER(10) NOT NULL,
     inf_sexo CHAR(6),
     inf_idade NUMBER(3) NOT NULL,
     inf_rac_id NUMBER(10) NOT NULL,
@@ -58,12 +48,10 @@ CREATE TABLE infratores (
 );
 
 CREATE TABLE relacionamentos (
-    rel_id NUMBER(10) NOT NULL,
     rel_relacao VARCHAR(32)
 );
 
 CREATE TABLE homicidios (
-    hmc_id NUMBER(10) NOT NULL,
     hmc_recorde CHAR(6) NOT NULL,
     hmc_qtd_infratores NUMBER(2) NOT NULL,
     hmc_solucao VARCHAR(3),
